3cefb2efa7806e221f5465f0d617676e79c68628
Yevhen Malyi <yevhen.malyi@indeema.com>
2023-07-25 22:16:11 +0300

    added paragraphs generator

--- src/app/features/thread/components/processed-thread/processed-thread.component.ts
+++ src/app/features/thread/components/processed-thread/processed-thread.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { ThreadsService } from '../../services';
 
 @Component({
   selector: 'app-processed-thread',
   templateUrl: './processed-thread.component.html',
   styleUrls: ['./processed-thread.component.scss']
 })
-export class ProcessedThreadComponent {}
+export class ProcessedThreadComponent implements OnInit {
+  thread: string[] = [];
+  destroyRef = inject(DestroyRef);
+
+  constructor(private threadsService: ThreadsService) {}
+
+  ngOnInit(): void {
+    this.threadsService.get()
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(next => {
+        console.log(next);
+        this.thread = next;
+      });
+  }
+}
--- src/app/features/thread/components/processed-thread/processed-thread.module.ts
+++ src/app/features/thread/components/processed-thread/processed-thread.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProcessedThreadComponent } from './processed-thread.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   exports: [ProcessedThreadComponent],
   declarations: [ProcessedThreadComponent],
   providers: [],
